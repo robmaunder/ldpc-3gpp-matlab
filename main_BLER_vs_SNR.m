b89433d2e7d5b0e8d1aad6f87ad7871d3587b4e4
Rob Maunder <rob.maunder@accelercomm.com>
2018-07-25 22:51:03 +0100

    Added support for other CRCs

--- main_BLER_vs_SNR.m
+++ main_BLER_vs_SNR.m
@@ -51,136 +51,135 @@ hMod = comm.PSKModulator(modulation_order, 'BitInput',true);
 
 % Consider each base graph in turn
 for BG_index = 1:length(BG)
-        for E_index = 1:length(E)
+    for E_index = 1:length(E)
+        
+        % Create a figure to plot the results.
+        figure
+        axes1 = axes('YScale','log');
+        title(['3GPP New Radio LDPC code, BG',num2str(BG(BG_index)),', ',CRC,', E = ',num2str(E(E_index)),', iterations = ',num2str(iterations),', errors = ',num2str(target_block_errors),', QPSK, AWGN']);
+        ylabel('BLER');
+        xlabel('E_s/N_0 [dB]');
+        ylim([target_BLER,1]);
+        hold on
+        drawnow
+        
+        % Consider each information block length in turn
+        for K_prime_minus_L_index = 1:length(K_prime_minus_L)
             
-            % Create a figure to plot the results.
-            figure
-            axes1 = axes('YScale','log');
-            title(['3GPP New Radio LDPC code, BG',num2str(BG(BG_index)),', ',CRC,', E = ',num2str(E(E_index)),', iterations = ',num2str(iterations),', errors = ',num2str(target_block_errors),', QPSK, AWGN']);
-            ylabel('BLER');
-            xlabel('E_s/N_0 [dB]');
-            ylim([target_BLER,1]);
-            hold on
-            drawnow
+            % Create the plot
+            plot1 = plot(nan,'Parent',axes1);
+            legend(cellstr(num2str(K_prime_minus_L(1:K_prime_minus_L_index)', 'K_prime_minus_L=%d')),'Location','southwest');
             
-            % Consider each information block length in turn
-            for K_prime_minus_L_index = 1:length(K_prime_minus_L)
-                
-                % Create the plot
-                plot1 = plot(nan,'Parent',axes1);
-                legend(cellstr(num2str(K_prime_minus_L(1:K_prime_minus_L_index)', 'K_prime_minus_L=%d')),'Location','southwest');
-                
-                % Counters to store the number of bits and errors simulated so far
-                block_counts=[];
-                block_error_counts=[];
-                EsN0s = [];
-                
-                % Open a file to save the results into.
-                filename = ['results/BLER_vs_SNR_',num2str(BG(BG_index)),'_',CRC,'_',num2str(K_prime_minus_L(K_prime_minus_L_index)),'_',num2str(E(E_index)),'_',num2str(iterations),'_',num2str(target_block_errors),'_',num2str(seed)];
-                fid = fopen([filename,'.txt'],'w');
-                if fid == -1
-                    error('Could not open %s.txt',filename);
-                end
-                
-                % Initialise the BLER and SNR
-                BLER = 1;
-                EsN0 = EsN0_start;
-                
-                found_start = false;
-                
-                % Skip any encoded block lengths that generate errors
-                try
-                    
-                    hEnc = NRLDPCEncoder('BG',BG(BG_index),'CRC',CRC,'K_prime_minus_L',K_prime_minus_L(K_prime_minus_L_index),'E',E(E_index))
-                    hDec = NRLDPCDecoder('BG',BG(BG_index),'CRC',CRC,'K_prime_minus_L',K_prime_minus_L(K_prime_minus_L_index),'E',E(E_index),'I_HARQ',1,'iterations',iterations);
-                    
-                    
-                    % Loop over the SNRs
-                    while BLER > target_BLER
-                        hChan = comm.AWGNChannel('NoiseMethod','Signal to noise ratio (SNR)','SNR',EsN0);
-                        hDemod = comm.PSKDemodulator(modulation_order, 'BitOutput',true, 'DecisionMethod','Log-likelihood ratio', 'Variance', 1/10^(hChan.SNR/10));
-                        
-                        
-                        % Start new counters
-                        block_counts(end+1) = 0;
-                        block_error_counts(end+1) = 0;
-                        EsN0s(end+1) = EsN0;
-                        
-                        keep_going = true;
-                        
-                        % Continue the simulation until enough block errors have been simulated
-                        while keep_going && block_error_counts(end) < target_block_errors
-                            
-                            b = round(rand(K_prime_minus_L(K_prime_minus_L_index),1));
-                            
-                            b_hat = [];
-                            rv_id_index = 1;
-                            reset(hDec); % Reset the incremental redundancy buffer
-                            
-                            while isempty(b_hat) && rv_id_index <= length(rv_id_sequence)
-                            
-                                hEnc.rv_id = rv_id_sequence(rv_id_index);
-                                hDec.rv_id = rv_id_sequence(rv_id_index);
-                                                               
-                                d = step(hEnc,b);
-                                e = d(~isnan(d));
-                                f = [e;zeros(mod(-length(e),log2(modulation_order)),1)];
-                                tx = step(hMod, f);
-                                rx = step(hChan, tx);
-                                f_tilde = step(hDemod, rx);
-                                e_tilde = f_tilde(1:length(e));
-                                d_tilde = d;
-                                d_tilde(~isnan(d)) = e_tilde;
-                                b_hat = step(hDec, d_tilde);
-                            
-                                rv_id_index = rv_id_index + 1;
-                            end
-                            
-                            if found_start == false && ~isequal(b,b_hat)
-                                keep_going = false;
-                                BLER = 1;
-                            else
-                                found_start = true;
-                                
-                                % Determine if we have a block error
-                                if ~isequal(b,b_hat)
-                                    block_error_counts(end) = block_error_counts(end) + 1;
-                                end
-                                
-                                % Accumulate the number of blocks that have been simulated
-                                % so far
-                                block_counts(end) = block_counts(end) + 1;
-                                
-                                % Calculate the BLER and save it in the file
-                                BLER = block_error_counts(end)/block_counts(end);
-                                
-                                % Plot the BLER vs SNR results
-                                set(plot1,'XData',EsN0s);
-                                set(plot1,'YData',block_error_counts./block_counts);
-                                drawnow
-                            end
-                        end
-                        
-                        if BLER < 1
-                            fprintf(fid,'%f\t%e\n',EsN0,BLER);
-                        end
-                        
-                        % Update the SNR, ready for the next loop
-                        EsN0 = EsN0 + EsN0_delta;
-                        
-                    end
-                catch ME
-                    if strcmp(ME.identifier, 'ldpc_3gpp_matlab:UnsupportedBlockLength')
-                        warning('ldpc_3gpp_matlab:UnsupportedBlockLength','The combination of base graph BG=%d and K_prime_minus_L=%d is not supported. %s',BG(BG_index),K_prime_minus_L(K_prime_minus_L_index), getReport(ME, 'basic', 'hyperlinks', 'on' ));
-                        continue
-                    else
-                        rethrow(ME);
-                    end
-                end
-                
-                % Close the file
-                fclose(fid);
+            % Counters to store the number of bits and errors simulated so far
+            block_counts=[];
+            block_error_counts=[];
+            EsN0s = [];
+            
+            % Open a file to save the results into.
+            filename = ['results/BLER_vs_SNR_',num2str(BG(BG_index)),'_',CRC,'_',num2str(K_prime_minus_L(K_prime_minus_L_index)),'_',num2str(E(E_index)),'_',num2str(iterations),'_',num2str(target_block_errors),'_',num2str(seed)];
+            fid = fopen([filename,'.txt'],'w');
+            if fid == -1
+                error('Could not open %s.txt',filename);
             end
+            
+            % Initialise the BLER and SNR
+            BLER = 1;
+            EsN0 = EsN0_start;
+            
+            found_start = false;
+            
+            % Skip any encoded block lengths that generate errors
+            try
+                
+                hEnc = NRLDPCEncoder('BG',BG(BG_index),'CRC',CRC,'K_prime_minus_L',K_prime_minus_L(K_prime_minus_L_index),'E',E(E_index))
+                hDec = NRLDPCDecoder('BG',BG(BG_index),'CRC',CRC,'K_prime_minus_L',K_prime_minus_L(K_prime_minus_L_index),'E',E(E_index),'I_HARQ',1,'iterations',iterations);
+                
+                
+                % Loop over the SNRs
+                while BLER > target_BLER
+                    hChan = comm.AWGNChannel('NoiseMethod','Signal to noise ratio (SNR)','SNR',EsN0);
+                    hDemod = comm.PSKDemodulator(modulation_order, 'BitOutput',true, 'DecisionMethod','Log-likelihood ratio', 'Variance', 1/10^(hChan.SNR/10));
+                    
+                    
+                    % Start new counters
+                    block_counts(end+1) = 0;
+                    block_error_counts(end+1) = 0;
+                    EsN0s(end+1) = EsN0;
+                    
+                    keep_going = true;
+                    
+                    % Continue the simulation until enough block errors have been simulated
+                    while keep_going && block_error_counts(end) < target_block_errors
+                        
+                        b = round(rand(K_prime_minus_L(K_prime_minus_L_index),1));
+                        
+                        b_hat = [];
+                        rv_id_index = 1;
+                        reset(hDec); % Reset the incremental redundancy buffer
+                        
+                        while isempty(b_hat) && rv_id_index <= length(rv_id_sequence)
+                            
+                            hEnc.rv_id = rv_id_sequence(rv_id_index);
+                            hDec.rv_id = rv_id_sequence(rv_id_index);
+                            
+                            d = step(hEnc,b);
+                            e = d(~isnan(d));
+                            f = [e;zeros(mod(-length(e),log2(modulation_order)),1)];
+                            tx = step(hMod, f);
+                            rx = step(hChan, tx);
+                            f_tilde = step(hDemod, rx);
+                            e_tilde = f_tilde(1:length(e));
+                            d_tilde = d;
+                            d_tilde(~isnan(d)) = e_tilde;
+                            b_hat = step(hDec, d_tilde);
+                            
+                            rv_id_index = rv_id_index + 1;
+                        end
+                        
+                        if found_start == false && ~isequal(b,b_hat)
+                            keep_going = false;
+                            BLER = 1;
+                        else
+                            found_start = true;
+                            
+                            % Determine if we have a block error
+                            if ~isequal(b,b_hat)
+                                block_error_counts(end) = block_error_counts(end) + 1;
+                            end
+                            
+                            % Accumulate the number of blocks that have been simulated
+                            % so far
+                            block_counts(end) = block_counts(end) + 1;
+                            
+                            % Calculate the BLER and save it in the file
+                            BLER = block_error_counts(end)/block_counts(end);
+                            
+                            % Plot the BLER vs SNR results
+                            set(plot1,'XData',EsN0s);
+                            set(plot1,'YData',block_error_counts./block_counts);
+                            drawnow
+                        end
+                    end
+                    
+                    if BLER < 1
+                        fprintf(fid,'%f\t%e\n',EsN0,BLER);
+                    end
+                    
+                    % Update the SNR, ready for the next loop
+                    EsN0 = EsN0 + EsN0_delta;
+                    
+                end
+            catch ME
+                if strcmp(ME.identifier, 'ldpc_3gpp_matlab:UnsupportedBlockLength')
+                    warning('ldpc_3gpp_matlab:UnsupportedBlockLength','The combination of base graph BG=%d and K_prime_minus_L=%d is not supported. %s',BG(BG_index),K_prime_minus_L(K_prime_minus_L_index), getReport(ME, 'basic', 'hyperlinks', 'on' ));
+                    continue
+                else
+                    rethrow(ME);
+                end
+            end
+            
+            % Close the file
+            fclose(fid);
         end
     end
 end
